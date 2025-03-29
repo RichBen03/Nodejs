6a2979c29941fa473f65c3f5fe6c83e1ba61cc9c
Rich Benjamin <richmwendwa03@gmail.com>
2025-03-29 23:57:35 +0300

    A program that executes step by step

---
+++ Async/synchronous.js
@@ -0,0 +1,9 @@
+// Synchronous JS= executes from step one to last step in a series, cannot proceed unless step one is completed
+console.log("I")
+console.log("eat")
+console.log("ice cream")
+console.log("with")
+console.log("a spoon")
+
+
+
