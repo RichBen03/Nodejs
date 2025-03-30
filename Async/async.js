a269149f8eeb0ea7b66908f1ef5a899bcf73e954
Rich Benjamin <richmwendwa03@gmail.com>
2025-03-30 14:48:44 +0300

    used the inbuilt function setTimeout to implement asynchronous js

---
+++ Async/async.js
@@ -0,0 +1,13 @@
+setTimeout(()=>{
+    console.log("Washing my eyes")
+
+},3000)
+
+setTimeout(()=>{
+    console.log("Wiping water off my face")
+
+},4000)
+
+setTimeout(()=>{
+    console.log("Drinking water to stay hydrated")
+},7000)
